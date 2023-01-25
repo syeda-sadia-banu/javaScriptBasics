cd49a6f4bd8b70a55b7d9ae99617b2fb8878372e
syeda-sadia-banu <sadia.banu17@gmail.com>
2023-01-25 12:56:09 +0530

    modified'

--- game.js
+++ game.js
@@ -1,7 +1,36 @@
-let cards=[6,7];
-let sum=cards[0]+cards[1];
+// let firstCard=getRandomCard();
+// let secondCard=getRandomCard();
+
+// let cards=[firstCard,secondCard];
+let cards=[]
+ let sum= 0;//firstCard+secondCard;
 let message="";
+let isAlive=false;
+let hasBlackJack=false;
+let player={
+    name:"per",
+    chips:200
+}
+document.getElementById("player-el").textContent=player.name+":$"+player.chips;
+
+function getRandomCard(){
+    let randomNumber= Math.floor(Math.random()*13)+1;
+    if(randomNumber>10){
+        return 10
+    }else if(randomNumber===1){
+        return 11
+    }else{
+        return randomNumber
+    }
+}
+
+
 function startGame(){
+    isAlive=true;
+    let firstCard=getRandomCard();
+    let secondCard=getRandomCard();
+    cards=[firstCard,secondCard];
+    sum=firstCard+secondCard;
     renderGame();
 }
 
@@ -26,21 +55,26 @@ function renderGame(){
      else if(sum===21){
         // console.log("wohoo! you'have got blackjack🥳")
         message="wohoo! you'have got blackjack🥳";
+        hasBlackJack=true;
      }
      else {
         // console.log("you're out of the game!😔")
          message="you're out of the game!😔"
+         isAlive-false;
      }
      document.getElementById("message-el").textContent=message;
 }
 function newCard(){
-    let card=2;
-    cards.push(card);
+    if(isAlive===true && hasBlackJack===false){
+        let card=getRandomCard();
+        cards.push(card);
+       
+        sum+=card;
+      
+        renderGame();
+        console.log(cards)
+    }
    
-    sum+=card;
-    console.log(cards)
-    renderGame();
-    console.log(cards)
 }
 console.log(cards)
 
